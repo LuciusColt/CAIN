37f6e14530c2fbf0e6e9bd984023305c64d0c8b1
LuciusColt <lucian.justus@gmail.com>
2025-11-06 16:26:03 -0600

    Quartz sync: Nov 6, 2025, 4:26 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -42,7 +42,7 @@ export const defaultContentPageLayout: PageLayout = {
   title: "Explorer",
   folderClickBehavior: "collapse",
   folderDefaultState: "open",                
-  useSavedState: false,                       
+  useSavedState: true,                       
   order: ["filter", "map", "sort"],
 }),
   ],
