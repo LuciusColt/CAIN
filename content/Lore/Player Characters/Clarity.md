402a7c633aba6df6e1d316bbc46dc7eaa196aedf
LuciusColt <lucian.justus@gmail.com>
2025-11-12 21:01:32 -0600

    Quartz sync: Nov 12, 2025, 9:01 PM

--- content/Lore/Player Characters/Clarity.md
+++ content/Lore/Player Characters/Clarity.md
@@ -1,8 +1,9 @@
 ---
 title: CLARITY
 tags:
-  - Virtue
 draft: true
 ---
+**VIRTUE**
+
 "There are worse fates that death I can offer you, I've authorized them all."
 "The world offered me divinity, I chose something far more dangerous... Clarity."
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -42,7 +42,7 @@ export const defaultContentPageLayout: PageLayout = {
   title: "Explorer",
   folderClickBehavior: "collapse",
   folderDefaultState: "collapsed",                
-  useSavedState: false,                       
+  useSavedState: true,                       
   order: ["filter", "map", "sort"],
 }),
   ],
